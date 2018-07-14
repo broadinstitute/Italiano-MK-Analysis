bd2d85267f5eb79f26f5fd616510859431683410
Prakrith <prakrith@brandeis.edu>
2018-07-13 23:31:22 -0400

    180713_Update
    MK_Analysis_v3

---
+++ hpc_scripts/MouseMK.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -c 8                             
+#SBATCH -N 1                               
+#SBATCH -t 7-00:00                         
+#SBATCH -p long                          
+#SBATCH --mem=4G                          
+
+cd /n/data2/bwh/medicine/italiano/Inucycte_Analysis/
+conda activate bioimg
+python MouseMK.py
